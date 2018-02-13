7a92fac07f86b3beadde7e130401e16936f5a86d
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-02-13 15:13:08 +0000

    added CalculateZoom method

--- src/chip8/Backend.h
+++ src/chip8/Backend.h
@@ -1,6 +1,9 @@
 #ifndef BACKEND_H
 #define BACKEND_H
 
+#include <chip8/types.h>
+#include <algorithm>
+
 namespace chip8
 {
 	class Framebuffer;
@@ -10,6 +13,26 @@ namespace chip8
 	public:
 		virtual void Render(const Framebuffer & fb) = 0;
 		virtual ~Backend() { }
+
+
+		static void CalculateZoom(int &num, int &denom, int & offsetX, int &offsetY, int displayW, int displayH, int chipW, int chipH)
+		{
+			if (displayW >= chipW && displayH >= chipH) //zoom out
+			{
+				num = std::min(displayW / chipW, displayH / chipH);
+				denom = 1;
+				offsetX = (displayW - chipW * num) / 2;
+				offsetY = (displayH - chipH * num) / 2;
+			}
+			else {
+				//test me
+				num = 1;
+				denom = std::max((chipW + displayW - 1) / displayW, (displayH + chipH - 1) / chipH);
+				offsetX = (displayW - chipW / denom) / 2;
+				offsetY = (displayH - chipH / denom) / 2;
+			}
+		}
+
 	};
 }
 
