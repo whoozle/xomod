74a7d55d87cc2abd1e22ac000bc744c203250b96
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-02-13 22:44:17 +0000

    cleaned up mess, added background color

--- src/chip8/backend/terminal/TerminalBackend.cpp
+++ src/chip8/backend/terminal/TerminalBackend.cpp
@@ -9,42 +9,32 @@ namespace chip8
 	void TerminalBackend::Render(Framebuffer & fb)
 	{
 		struct winsize w;
-		int chipW = fb.GetWidth();
-		int chipH = fb.GetHeight();
-
-		u8 * data = fb.GetLine(0);
-		printf("PAINT\n");
-		for(int i = 0; i < chipH; ++i) {
-			for(int j = 0; j < chipW; ++j) {
-				printf("%c", (*data & ~Framebuffer::DirtyBit) + '0');
-				++data;
-			}
-			printf("\n");
-		}
-
 		if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) != 0)
 			return;
 
-		printf("terminal size %dx%d, fbsize: %dx%d\n", w.ws_col, w.ws_row, chipW, chipH);
 		if (w.ws_col <= 0 || w.ws_row <= 0)
 			return;
 
-//		return;
+		int chipW = fb.GetWidth();
+		int chipH = fb.GetHeight();
+
+		//printf("terminal size %dx%d, fbsize: %dx%d\n", w.ws_col, w.ws_row, chipW, chipH);
 
 		int num, denom, offsetX, offsetY;
 		CalculateZoom(num, denom, offsetX, offsetY, w.ws_col, w.ws_row, chipW, chipH);
-		//printf("%d %d %+d %+d\n", num, denom, offsetX, offsetY);
+
+		++offsetX;
+		++offsetY;
+
 		fputs("\033[s", stdout);
-		for(unsigned y = 0; y < chipH; ++y)
+		for(unsigned y = 0; y < chipH; y += denom, ++offsetY)
 		{
-			//printf("\033[%d;%df", y + offsetY + 1, offsetX + 1);
+			printf("\033[%d;%df", offsetY, offsetX);
 			auto line = fb.GetLine(y);
-			for(unsigned x = 0; x < chipW; ++x)
+			for(unsigned x = 0; x < chipW; x += denom)
 			{
 				auto value = line[x] & ~Framebuffer::DirtyBit;
-				if (value)
-					printf("%u, %u = %u\n", y, x, value);
-				//fputc('0' + value, stdout);
+				printf("\e[%dm ", value + 40);
 			}
 		}
 		fputs("\033[u", stdout);
